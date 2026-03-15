2d8bb8382f1e73bcc2828a21dc60101220449afd
Akira R Kinjo <993982+arkinjo@users.noreply.github.com>
2026-03-15 20:23:32 +0900

    Update run_docker.sh

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -4,7 +4,7 @@ PORT=${2:-"8001"}
 echo "Starting container with image: ${IMAGE} and port: ${PORT}"
 echo "NCBI_API_KEY: ${NCBI_API_KEY}"
 
-docker container run --rm -d -p ${PORT}:8000 \
+docker container run -d -p ${PORT}:8000 \
     -e NCBI_API_KEY=${NCBI_API_KEY} \
     --restart always \
     ${IMAGE} 
